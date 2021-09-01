84acdc46116be06e1e73c6431e29f253cf5666fc
Subhradip <subhradip.1992@gmail.com>
2021-09-01 13:18:21 +0530

    changes to forecast

--- views/site/forecast.php
+++ views/site/forecast.php
@@ -43,18 +43,24 @@ body {
 <div class="site-index">
 
     <div class="main-content">
+
     <div class="container-fluid px-1 px-md-4 py-5 mx-auto">
+
     <div id= "forecast" class="row d-flex justify-content-center px-3">
+
         <div class="card">
             <h2 class="ml-auto mr-4 mt-3 mb-0"><?= $city;?></h2>
             <p class="ml-auto mr-4 mb-0 med-font"><?=$desc?></p>
-            <h1 class="ml-auto mr-4 large-font"><?= $temp?>&#176;</h1>
+            <h1 class="ml-auto mr-4 large-font"><?= $temp?>&#176;C</h1>
             <p class="time-font mb-0 ml-4 mt-auto"><?= $time?></p>
             <p class="ml-4 mb-4"><?=$date?></p>
         </div>
+
     </div>
-</div>
- </div>
+
+    </div>
+    
+    </div>
     
     
 </div>
